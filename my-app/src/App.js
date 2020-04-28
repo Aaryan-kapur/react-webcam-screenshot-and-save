efce573cfc85b83c485ccde38c7ea11b9023b1fb
Aaryan-kapur <ak6392@bennett.edu.in>
2020-04-29 03:15:48 +0530

    init

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Webcam from 'react-webcam';
+import axios from 'axios'
+export default class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = { screenshot: null }
+    }
+    screenshot() {
+        var screenshot = this.refs.webcam.getScreenshot();
+        this.setState({screenshot: screenshot});
+      }
+      onClickHandler = () => {
+        const data = new FormData()
+        data.append('file', this.state.selectedFile)
+        axios.post("http://localhost:8000/upload", data, {
+       })
+     .then(res => {
+         console.log(res.statusText)
+      })
+     }
+    render(){
+        return (
+            <div>   
+             <Webcam audio ={false} ref='webcam'/>
+             <button onClick={this.screenshot.bind(this)}>Capture</button>
+             { this.state.screenshot ? <img src={this.state.screenshot} /> : null }
+            </div>
+            )
+    }
+}
